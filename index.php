e3b0200813a242e6f85049968eaeb65fe4d87120
MatiRoss <mati04.ross@gmail.com>
2021-01-26 11:34:28 +0100

    v1.4

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
-echo "aaaaaaaaaaaaaa";
+error_reporting(E_ALL);
+ini_set('display_errors',true);
+ini_set('html_errors',false);
+ini_set('display_startup_errors',true);
+ini_set('log_errors',false);
 
-if (isset($_GET['action'])) {
+//Appel de la BDD
+require 'config/database.php';
+
+$map = [
+    'home' => 'homeController.php',
+    'post' => 'post.php',
+    '404' => '404.php',
+];
+if (filter_has_var(INPUT_GET, 'action')) {
     $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
-    if ($action == 'database') {
-        require(__DIR__ . '/config/database.php');
-    } else if ($action == '') {
-        require(__DIR__ . '');
-    } else {
-        require(__DIR__ . '/pages/404.php');
+    if (!isset($map[$action])) {
+        $action = '404';
     }
-
+    $fichier = $map[$action];
 } else {
-    require '';
+    $action = 'home';
 }
-
+include 'app/controllers/' . $fichier;
